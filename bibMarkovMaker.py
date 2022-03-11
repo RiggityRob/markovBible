4323bffbc38c2ebd0a907e116c2a812aa069ebae
RiggityRob <rgoodeve@gmail.com>
2022-03-11 15:13:34 -0500

    some file changes to make it windows friendly

--- bibMarkovMaker.py
+++ bibMarkovMaker.py
@@ -1,15 +1,10 @@
 import markovify
 from pathlib import Path
-
 #requires pip install of markovify
-
 files = Path("bible/").iterdir()
-
 for file in files:
 	with file.open('r') as file:
 		text = file.read()
-
 text_model = markovify.Text(text)
-
 for i in range(10):
 	print(text_model.make_sentence())
